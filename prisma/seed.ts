e4b36dcfa1c48d16be80851c585d40f4db2ce81f
Tomasz Dabrowski <tomixedex@icloud.com>
2026-04-28 20:38:39 +0200

    fix seed prisma adapter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const prisma = new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
 async function main() {
   await prisma.product.create({
     data: {
