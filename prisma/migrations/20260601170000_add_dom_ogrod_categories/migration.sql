292e30260f3b46f63ff38dab8c750a44592af603
Tomasz Dabrowski <tomixedex@icloud.com>
2026-06-01 19:40:23 +0200

    Add DOM and OGROD product categories

--- prisma/migrations/20260601170000_add_dom_ogrod_categories/migration.sql
+++ prisma/migrations/20260601170000_add_dom_ogrod_categories/migration.sql
@@ -1,4 +1,2 @@
--- Dodaje nowe wartości enum używane w kodzie: /produkty?category=DOM oraz /produkty?category=OGROD.
--- Bez tego PostgreSQL zwraca błąd dla wartości "DOM" / "OGROD" i endpoint /api/products kończy się 500.
 ALTER TYPE "ProductCategory" ADD VALUE IF NOT EXISTS 'DOM';
 ALTER TYPE "ProductCategory" ADD VALUE IF NOT EXISTS 'OGROD';
